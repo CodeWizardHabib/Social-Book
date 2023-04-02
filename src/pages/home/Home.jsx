954840321044cf254ecb3b9478b3a8d9649308ad
CodeWizardHabib <rehman.afzal@devsinc.com>
2023-04-03 01:18:15 +0500

    HomePage * I added hero slider * I added home about section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
-
+import HeroSlider from '../../components/ui/HeroSlider'
+import HomeAbout from './HomeAbout'
+import './Home.css'
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='home-page'>
+    <HeroSlider />
+    <HomeAbout />
+    </div>
   )
 }
